300bb0d2b952a1ca524238e0540da1ecd871a4f9
noxabellus <noxabellus@gmail.com>
2024-02-26 14:16:30 -0600

    init

---
+++ readme.md
@@ -0,0 +1,4 @@
+# @types/vscode-grammar
+
+Provides typescript types for use with
+[`vscode-grammar-builder`](https://github.com/noxabellus/vscode-grammar-builder)
---
+++ src/index.d.ts
@@ -0,0 +1,66 @@
+export interface JsonObject {
+    [key: string]: string | JsonObject | JsonObject[];
+}
+
+export interface JsonGrammarCaptures {
+    [key: string]: {
+        name: string;
+    };
+}
+export interface TMGrammarCaptures {
+    [key: number]: {
+        name: string;
+    };
+}
+
+export interface JsonGrammarScope {
+    name?: string;
+    comment?: string;
+    match?: string;
+    begin?: string;
+    end?: string;
+    captures?: JsonGrammarCaptures;
+    beginCaptures?: JsonGrammarCaptures;
+    endCaptures?: JsonGrammarCaptures;
+    contentName?: string;
+    patterns?: JsonGrammarScope;
+    include?: string;
+}
+export interface TMGrammarScope {
+    name?: string;
+    comment?: string;
+    match?: string | RegExp;
+    begin?: string | RegExp;
+    end?: string | RegExp;
+    captures?: TMGrammarCaptures;
+    beginCaptures?: TMGrammarCaptures;
+    endCaptures?: TMGrammarCaptures;
+    contentName?: string;
+    patterns?: TMGrammarScope[];
+    include?: string;
+}
+
+export interface JsonGrammar {
+    name: string;
+    scopeName: string;
+    injectionSelector?: string;
+    fileTypes?: string[];
+    keyEquivalent?: string;
+    uuid?: string;
+    patterns: JsonGrammarScope[];
+    repository: {
+        [key: string]: JsonGrammarScope;
+    };
+}
+export interface TMGrammar {
+    name: string;
+    scopeName: string;
+    injectionSelector?: string;
+    fileTypes?: string[];
+    keyEquivalent?: string;
+    uuid?: string;
+    patterns: TMGrammarScope[];
+    repository: {
+        [key: string]: TMGrammarScope;
+    };
+}
